113516bc7f9274157c173450ff3321f28972e615
euglazer <eugenestapler@gmail.com>
2015-05-11 10:35:30 +1200

    add load screen

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,4 +1,21 @@
-$(document).ready(function() {
+var loadInterval;
+
+$(document).ready(function () {
+  var deg = 0;
+  loadInterval = setInterval(function () {
+    $('#load img').css({
+      transform: 'rotateY(' + deg % 360 + 'deg)'
+    });
+    deg += 5;
+  }, 20);
+});
+
+$(window).load(function() {
+
+  clearInterval(loadInterval);
+  $('#load').hide();
+  $('#main').fadeIn(2000);
+
   var histogram = new Histogram($('#histogram'));
   var psychologist = new Psychologist(histogram);
   histogram.initialize();
